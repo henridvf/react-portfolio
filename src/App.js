4860fad43ade62505edf80e8756987ed387420eb
Henri <henridvf@gmail.com>
2021-12-09 12:12:28 +0000

    Revert /App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-blue-200 mx-auto rounded">
-      This my Portfolio
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
